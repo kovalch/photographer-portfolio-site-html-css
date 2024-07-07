ce35d73ef329803141ad2d8e150c0e87220c4eb7
kovalchn <marcstoever@web.de>
2024-07-07 13:28:40 +0200

    added checkbox and disclaimer for contact form

---
+++ typewriting-master/popup.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+                          console.log('JavaScript-Datei geladen'); // auskommentieren
+                          
+                          // JavaScript zum Öffnen und Schließen des Popups
+                          var openPopup = document.getElementById('open-popup');
+                          var closePopup = document.getElementById('close-popup');
+                          var popupOverlay = document.getElementById('popup-overlay');
+                          var popup = document.getElementById('popup');
+                          
+                          if (openPopup) {
+                          openPopup.addEventListener('click', function(event) {
+                                                     event.preventDefault();
+                                                     popup.style.display = 'block';
+                                                     popupOverlay.style.display = 'block';
+                                                     });
+                          }
+                          
+                          if (closePopup) {
+                          closePopup.addEventListener('click', function() {
+                                                      popup.style.display = 'none';
+                                                      popupOverlay.style.display = 'none';
+                                                      });
+                          }
+                          
+                          if (popupOverlay) {
+                          popupOverlay.addEventListener('click', function() {
+                                                        popup.style.display = 'none';
+                                                        popupOverlay.style.display = 'none';
+                                                        });
+                          }
+                          });
